f6d687ad134ea089ef4791d16600e9cdd2d8f5f4
thomasyu888 <thomas.yu@sagebase.org>
2021-11-17 07:18:25 -0800

    Add R exports and docs

---
+++ R/reexports.R
@@ -0,0 +1,26 @@
+
+
+#' @export
+reticulate::import
+
+#' @export
+reticulate::dict
+
+#' @export
+reticulate::tuple
+
+#' @export
+reticulate::iterate
+
+#' @export
+reticulate::`%as%`
+
+#' @export
+reticulate::use_python
+
+#' @export
+reticulate::use_virtualenv
+
+#' @export
+reticulate::use_condaenv
+
