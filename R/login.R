1997f14658765bdf8ac14f622df0d3b7d26162c4
thomasyu888 <thomas.yu@sagebase.org>
2021-11-17 08:45:35 -0800

    Add more parameters

--- R/login.R
+++ R/login.R
@@ -1,15 +1,34 @@
 #' Logs into Synapse.
 #'
-#' @param username a Synapse username (optional, not required if .synapseConfig available)
+#' @param email a Synapse username (optional, not required if .synapseConfig available)
 #' @param password a Synapse password (optional, not required if .synapseConfig available)
+#' @param apiKey Base64 encoded Synapse API key
+#' @param rememberMe Whether the authentication information should be cached in your operating system's credential storage.
+#' @param silent Defaults to False.  Suppresses the "Welcome ...!" message.
+#' @param forced  Defaults to False.  Bypass the credential cache if set.
+#' @param authToken A bearer authorization token, e.g. a personal access token, can be used in lieu of a password or apiKey
 #' @examples
-#' library(nfportalutils)
+#' library(synapserprototype)
 #' syn_login()
 #' @import reticulate
 #' @export
-syn_login <- function(username = NULL, password = NULL){
+syn_login <- function(
+  email=NULL,
+  password=NULL,
+  apiKey=NULL,
+  rememberMe=F,
+  silent=F,
+  forced=F,
+  authToken=NULL
+) {
   .syn <<- synapseclient$Synapse()
-  .syn$login(username, password)
+  .syn$login(email=email,
+             password=password,
+             apiKey=apiKey,
+             rememberMe=rememberMe,
+             silent=silent,
+             forced=forced,
+             authToken=authToken)
 }
 
 #' Checks .syn object exists.
